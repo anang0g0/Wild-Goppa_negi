c32c15d82fa3268c74a20e26c7c6e193d19c0fd2
anang0g0 <fumumue@protonmail.com>
2023-09-23 12:05:41 +0900

    mpro+simd

--- tokyo/marimo/v6.c
+++ tokyo/marimo/v6.c
@@ -353,8 +353,8 @@ int main() {
             int end_row = (i + 1) * rows_per_process;
 
 
-            //matrix_multiply_simd(AA,A_inv,shared_C,start_row,end_row);
-            matrix_multiply(AA, A_inv, shared_C, start_row, end_row);
+            matrix_multiply_simd(AA,A_inv,shared_C,start_row,end_row);
+            //matrix_multiply(AA, A_inv, shared_C, start_row, end_row);
 
             // 結果を表示
             printf("Process %d: Rows %d to %d completed\n", i, start_row, end_row);
